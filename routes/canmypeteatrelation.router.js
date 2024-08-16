7c2d2edfbf7db4bd694c917e3d06b996903e65c2
Luis Torres <ltorres661@gmail.com>
2024-08-16 08:46:29 +0200

    16/aug/24; main; Added files to host the application in plattform.sh

--- routes/canmypeteatrelation.router.js
+++ routes/canmypeteatrelation.router.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-//
 const canMyPetEatRelationController = require("../controllers/canMyPetEatRelation.controller");
 
 router.get(

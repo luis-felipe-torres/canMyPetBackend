28b719e4af3f19a1d24aeed17e34c9c98a8fc0ec
Luis Torres <ltorres661@gmail.com>
2024-08-17 09:12:35 +0200

    17/aug/24; main; Added files to host the application in plattform.sh

--- models/index.js
+++ models/index.js
@@ -8,17 +8,19 @@ const process = require("process");
 const basename = path.basename(__filename);
 const db = {};
 
-let sequelize = new Sequelize(
-  process.env.DATABASE,
+/*let sequelize = new Sequelize(
+  process.env.NAMEDATABASE,
   process.env.USERNAMEDATABASE,
   process.env.PASSWORDDATABASE,
   {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    dialect: process.env.DIALECT,
+    host: process.env.HOSTDATABASE,
+    port: process.env.PORTDATABASE,
+    dialect: process.env.DIALECTDATABASE,
   }
+);*/
+let sequelize = new Sequelize(
+  "mysql://avnadmin:AVNS_L-qXH1huSxaIgdBxbMr@canmypetbackend-canmypetbackend.i.aivencloud.com:24125/defaultdb?ssl-mode=REQUIRED"
 );
-
 sequelize
   .authenticate()
   .then(() => {
